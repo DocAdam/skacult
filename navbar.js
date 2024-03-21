86f7b1d5cb1d4155a42f5e83f490e9d68d6f0c15
Adam Pugh <adamthepugh@gmail.com>
2024-03-21 07:05:59 -0500

    Create navbar.js
    Navigation Bar for reuse across site

---
+++ navbar.js
@@ -0,0 +1,7 @@
+document.write(`
+<div class="navbar">
+    <a href="SKA_COC.html">Code of Conduct</a>
+    <a href="CULT_MUSIC.html">Cult Music</a>
+    <a href="CULT_TRUTHS.html">Cult Truths</a>
+</div>
+`);
