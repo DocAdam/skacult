bae387be59396d046bd5e8c90d5ed0841d78b2dc
Adam Pugh <adamthepugh@gmail.com>
2024-03-21 14:27:50 -0500

    Update navbar.js

--- navbar.js
+++ navbar.js
@@ -1,13 +1,9 @@
-/* Navbar Styles */
-.navbar {
-    width: 100%;
-    height: var(--navbar-height);
-    background-color: #333; /* Dark grey */
-    display: flex;
-    justify-content: center; /* Center the content inside the navbar */
-    align-items: center;
-    position: fixed; /* Keeps the navbar at the top */
-    top: 0;
-    z-index: 1000; /* Ensures navbar stays above other content */
-    padding: 0 20px; /* Add some padding on the sides */
-}
+document.write(`
+    <div class="navbar">
+        <a href="index.html">Cult Home</a>
+        <a href="SKA_COC.html">Cult of Conduct</a>
+        <a href="CULT_MUSIC.html">Cult Music</a>
+        <a href="CULT_TRUTHS.html">Cult Truths</a>
+        <a href="ABOUT.html">Cultish</a>
+    </div>
+`);
