5e71c78e74147bf147e00650b41e3bc8515962b0
Adam Pugh <adamthepugh@gmail.com>
2024-03-21 13:54:17 -0500

    Update navbar.js

--- navbar.js
+++ navbar.js
@@ -1,8 +1,9 @@
 document.write(`
 <div class="navbar">
-    <a href="ABOUT.html">Cult Home</a>
-    <a href="SKA_COC.html">Code of Conduct</a>
+    <a href="index.html">Cult Home</a>
+    <a href="SKA_COC.html">Cult of Conduct</a>
     <a href="CULT_MUSIC.html">Cult Music</a>
     <a href="CULT_TRUTHS.html">Cult Truths</a>
+    <a href="ABOUT.html">Cultish</a>
 </div>
 `);
