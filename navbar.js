d6a533bab67c3f3b9fbf2c69d2ab519719a34734
Adam Pugh <adamthepugh@gmail.com>
2026-02-21 06:19:26 -0600

    Refactor navbar structure and update links

--- navbar.js
+++ navbar.js
@@ -1,9 +1,14 @@
 document.write(`
-    <div class="navbar">
-        <a href="index.html">Cult Home</a>
-        <a href="SKA_COC.html">Cult of Conduct</a>
-        <a href="CULT_MUSIC.html">Cult Music</a>
-        <a href="CULT_TRUTHS.html">Cult Truths</a>
-        <a href="ABOUT.html">Cultish</a>
+  <nav class="navbar">
+    <div class="nav-left">
+      <a class="brand" href="index.html" aria-label="SKACULT home">SKACULT</a>
     </div>
+
+    <div class="nav-right">
+      <a href="index.html">Annex</a>
+      <a href="PROJECTS.html">Projects</a>
+      <a href="CONTACT.html">Contact</a>
+      <button class="nav-button" type="button" data-drawer-open="about">About</button>
+    </div>
+  </nav>
 `);
